047be71e09a3574503c00c3a3baeb3d46993a41d
Victor Luiz <victorluizcefet@gmail.com>
2020-12-11 19:58:42 -0300

    Definindo model de postagens

---
+++ models/Postagem.js
@@ -0,0 +1,32 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const Postagem = new Schema({
+    titulo: {
+        type: String,
+        required: true
+    },
+    slug: {
+        type: String,
+        required: true
+    },
+    descricao: {
+        type: String,
+        required: true
+    }, 
+    conteudo: {
+        type: String,
+        required: true
+    },
+    categoria: {
+        type: Schema.Types.ObjectId,
+        ref: 'categorias',
+        required: true
+    },
+    data: {
+        type: Date,
+        default: Date.now()
+    }
+});
+
+mongoose.model('postagens', Postagem);

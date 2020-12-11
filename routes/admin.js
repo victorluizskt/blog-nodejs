9bc75b0eae7dc7996a8e352a99c681ed9742618c
Victor Luiz <victorluizcefet@gmail.com>
2020-12-11 10:03:24 -0300

    Validando formulários com express

--- routes/admin.js
+++ routes/admin.js
@@ -26,15 +26,38 @@ const Categoria = mongoose.model('categorias');
 
 // Rota responsável por cadastrar o formulário dentro do mongo
     router.post('/categorias/nova', (req, res) => {
-        const novaCategoria = {
-            nome: req.body.nome,
-            slug: req.body.slug
-        }
-        new Categoria(novaCategoria).save().then(() => {
-            console.log('Categoria salva com sucesso');
-        }).catch((err) => {
-            console.log('Erro ao salvar categoria.');
-        });
+        var erros = [];
+
+        if(!req.body.nome || typeof req.body.nome === undefined || req.body.nome === null) {
+            erros.push({texto: 'Nome inválido.'});
+        };
+
+        if(!req.body.slug || typeof req.body.slug === undefined || req.body.slug === null){
+            erros.push({texto: 'Slug inválido.'})
+        };
+
+        if(req.body.nome.length < 2){
+            erros.push({texto: 'Nome da categoria é muito pequeno.'})
+        };
+
+        if(erros.length > 0){ 
+            res.render('admin/addcategorias', {
+                erros: erros
+            });
+        } else {
+            const novaCategoria = {
+                nome: req.body.nome,
+                slug: req.body.slug
+            };
+            new Categoria(novaCategoria).save().then(() => {
+                req.flash('success_msg', 'Categoria criada com sucesso.');
+                res.redirect('/admin/categorias');
+            }).catch((err) => {
+                req.flash('error_msg', 'Houve um erro ao salvar a categoria.');
+                res.redirect('/admin');
+            });
+        };
+
     });
 
 module.exports = router;

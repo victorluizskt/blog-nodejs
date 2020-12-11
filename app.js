cb7597dadf4c8e2a70ff5c7f654c7b6eab616abf
Victor Luiz <victorluizcefet@gmail.com>
2020-12-11 09:37:33 -0300

    Configurando sessões

--- app.js
+++ app.js
@@ -6,14 +6,28 @@
     const admin = require("./routes/admin");
     const path = require('path');
     const mongoose = require('mongoose');
-
+    const session = require('express-session');
+    const flash = require('connect-flash');
 // Configurações
+    // Sessão
+        app.use(session({
+            secret: 'cursodenode',
+            resave: true,
+            saveUninitialized: true
+        }));
+        app.use(flash());
+    // Middleware
+        app.use((req, res, next) => {
+            res.locals.success_msg = req.flash('success_msg');
+            res.locals.error_msg = req.flash('error_msg')
+            next();
+        });
     // Body Parser
-    app.use(bodyParser.urlencoded({extended: true}));
-    app.use(bodyParser.json());
+        app.use(bodyParser.urlencoded({extended: true}));
+        app.use(bodyParser.json());
     // HandleBars
-    app.engine('handlebars', handlebars({defaultLayout: 'main'}));
-    app.set('view engine', 'handlebars');
+        app.engine('handlebars', handlebars({defaultLayout: 'main'}));
+        app.set('view engine', 'handlebars');
     // Mongoose
         mongoose.Promise = global.Promise;
         mongoose.connect('mongodb://localhost/blogapp', {
@@ -25,8 +39,11 @@
             console.log('Erro ao se conectar: ' + err);
         });
     // Public (arquivos estáticos)
-    app.use(express.static(path.join(__dirname, 'public')));
-
+        app.use(express.static(path.join(__dirname, 'public')));
+        app.use((req, res, next) => {
+        console.log('Oi eu sou um Middlewar');
+        next();
+    });
 
 // Rotas
     app.use('/admin', admin);

0fb3f7b42bb562b88375790cf71aca4c47491fd7
Victor Luiz <victorluizcefet@gmail.com>
2020-12-11 09:06:24 -0300

    Cadastrando categorias no banco de dados

--- routes/admin.js
+++ routes/admin.js
@@ -1,5 +1,8 @@
 const express = require('express');
 const router = express.Router();
+const mongoose = require('mongoose');
+require('../models/Categoria');
+const Categoria = mongoose.model('categorias');
 
 // Rota principal para o painel administrativo
     router.get('/', (req, res) => {
@@ -19,6 +22,19 @@ const router = express.Router();
 // Rota para o formulário
     router.get('/categorias/add', (req, res) => {
         res.render('admin/addcategorias');
+    });
+
+// Rota responsável por cadastrar o formulário dentro do mongo
+    router.post('/categorias/nova', (req, res) => {
+        const novaCategoria = {
+            nome: req.body.nome,
+            slug: req.body.slug
+        }
+        new Categoria(novaCategoria).save().then(() => {
+            console.log('Categoria salva com sucesso');
+        }).catch((err) => {
+            console.log('Erro ao salvar categoria.');
+        });
     });
 
 module.exports = router;

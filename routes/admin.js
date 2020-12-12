b26bd5747ef9d48c764c66ea862e83d308f948d1
Victor Luiz <victorluizcefet@gmail.com>
2020-12-11 22:06:32 -0300

    Editando postagens

--- routes/admin.js
+++ routes/admin.js
@@ -179,4 +179,47 @@ const Postagem = mongoose.model('postagens');
         };
     });
 
+// Rota para a edição de posts
+    router.get('/postagens/edit/:id', (req, res) => {
+
+        Postagem.findOne({_id: req.params.id}).lean().then(postagem => {
+
+            Categoria.find().lean().then(categorias => {
+                res.render('admin/editpostagens', {categorias: categorias, postagem: postagem});
+            }).catch(err => {
+                req.flash('error_msg', 'Houve um erro ao listar as categorias.');
+                res.redirect('/admin/postagens');
+            });
+
+        }).catch(err => {
+            req.flash('error_msg', 'Houve um erro ao carregar o formulário de edição.');
+            res.redirect('/admin/postagens');
+        });
+    });
+
+// Rota para o salvamento do post
+    router.post('/postagem/edit', (req, res) => {
+        
+        Postagem.findOne({_id: req.body.id}).then(postagem => {
+            postagem.titulo = req.body.titulo;
+            postagem.slug = req.body.slug;
+            postagem.descricao = req.body.descricao;
+            postagem.conteudo = req.body.conteudo;
+            postagem.categoria = req.body.categoria;
+
+            postagem.save().then(() =>{
+                req.flash('success_msg', 'Postagem editada com sucesso.')
+                res.redirect('/admin/postagens')
+            }).catch(() => {
+                req.flash('error_msg', 'Erro ao editar categoria..')
+                res.redirect('/admin/categorias')
+            });
+
+        }).catch(() => {
+            req.flash('error_msg', 'Houve um erro ao salvar a edição.');
+            res.redirect('/admin/postagens');
+        });
+    });
+
+
 module.exports = router;

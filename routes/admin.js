fbecfe5ec2d49d5d1a7ae88c4c439cf7c1ca84df
Victor Luiz <victorluizcefet@gmail.com>
2020-12-11 21:18:40 -0300

    Salvando as postagens no banco de dados

--- routes/admin.js
+++ routes/admin.js
@@ -3,6 +3,8 @@ const router = express.Router();
 const mongoose = require('mongoose');
 require('../models/Categoria');
 const Categoria = mongoose.model('categorias');
+require('../models/Postagem');
+const Postagem = mongoose.model('postagens');
 
 // Rota principal para o painel administrativo
     router.get('/', (req, res) => {
@@ -139,6 +141,35 @@ const Categoria = mongoose.model('categorias');
         }).catch(err => {
             req.flash('error_msg', 'Houve um erro ao carregar o formulário');
         });
+    });
+    
+// Rota para salvar postagens no banco de dados
+    router.post('/postagens/nova', (req, res) => {
+        // tenho que fazer validação
+        var erros = [];
+
+        if(req.body.categoria === '0'){
+            erros.push({texto: 'Categoria inválida, registre uma categoria.'});
+        };
+
+        if(erros.length > 0){
+            res.render('admin/addpostagem', {erros: erros});
+        } else {
+            const novaPostagem = {
+                titulo: req.body.titulo,
+                descricao: req.body.descricao,
+                conteudo: req.body.conteudo,
+                categoria: req.body.categoria,
+                slug: req.body.slug
+            }
+            new Postagem(novaPostagem).save().then(() => {
+                req.flash('success_msg', 'Postagem criada com sucesso.');
+                res.redirect('/admin/postagens');
+            }).catch((err) => {
+                req.flash('error_msg', 'Houve um erro durante o salvamento da postagem');
+                res.redirect('/admin/postagens')
+            });
+        };
     });
 
 module.exports = router;

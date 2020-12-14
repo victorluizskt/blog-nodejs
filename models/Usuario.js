1505e0df0daccc5da3910144e676027548975a5c
Victor Luiz <victorluizcefet@gmail.com>
2020-12-14 13:50:17 -0300

    Definindo model de usuarios

---
+++ models/Usuario.js
@@ -0,0 +1,23 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const Usuario = new Schema ({
+    nome: {
+        type: String,
+        required: true
+    },
+    cpf: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true
+    },
+    senha: {
+        type: String,
+        required: true
+    }
+});
+
+mongoose.model('usuarios', Usuario);

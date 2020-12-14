54f4d5fa44c7dfd4e2ffd0ceeb0b8c0a8589efca
Victor Luiz <victorluizcefet@gmail.com>
2020-12-14 14:34:21 -0300

    Cadastrando usuarios no banco de dados

--- routes/usuario.js
+++ routes/usuario.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const mongoose = require('mongoose');
 require('../models/Usuario');
 const Usuario = mongoose.model('usuarios');
+const bcrypt = require('bcryptjs');
 
 router.get('/registro', (req, res) => {
     res.render('usuarios/registro');
@@ -35,7 +36,42 @@ router.post('/registro', (req, res) => {
     if(erros.length > 0){
         res.render('usuarios/registro', {erros: erros});
     } else {
-        
+        Usuario.findOne({email: req.body.email}).then((usuario) => {
+            if(usuario){
+                req.flash('error_msg', 'Já existe uma conta com esse email no sistema.');
+                res.redirect('/usuarios/registro');
+            } else {
+
+                const novoUsuario = new Usuario({
+                    nome: req.body.nome,
+                    email: req.body.email,
+                    senha: req.body.senha
+                });
+
+                bcrypt.genSalt(10, (erro, salt) => { 
+                    bcrypt.hash(novoUsuario.senha, salt, (erro, hash) => {
+                        if(erro) {
+                            req.flash('error_msg', ' Houve um erro durante o salvamento do usuário.');
+                            res.redirect('/');
+                        }
+
+                        novoUsuario.senha = hash;
+
+                        novoUsuario.save().then(() => {
+                            req.flash('success_msg', 'Usuário criado com sucesso.');
+                            res.redirect('/');
+                        }).catch(err => {
+                            req.flash('error_msg', 'Houve um erro ao criar o usuário');
+                            res.redirect('/usuarios/registro');
+                        });
+                    });
+                });
+
+            }
+        }).catch(err => {
+            req.flash('error_msg', 'Houve um erro interno.');
+            res.redirect('/')
+        });
     }
 });
 

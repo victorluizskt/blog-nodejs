7ff93d0e3046afc59ad5bc767b2a98a54091961a
Victor Luiz <victorluizcefet@gmail.com>
2020-12-14 09:44:41 -0300

    Deletando postagens

--- routes/admin.js
+++ routes/admin.js
@@ -221,5 +221,15 @@ const Postagem = mongoose.model('postagens');
         });
     });
 
+// Apagando postagem pelo metódo get, não recomendado.
+    router.get('/postagens/deletar/:id', (req, res) => {
+        Postagem.remove({_id: req.params.id}).then(() => {
+            req.flash('success_msg', 'Postagem deletada com sucesso.');
+            res.redirect('/admin/postagens');
+        }).catch((err) => {
+            req.flash('error_msg', 'Houve um erro interdo');
+            res.redirect('admin/postagens');
+        });
+    });
 
 module.exports = router;

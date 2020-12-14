814b72236669dbad65fbb7d151dd174740a4f68b
Victor Luiz <victorluizcefet@gmail.com>
2020-12-14 19:43:03 -0300

    initial commit

--- config/db.js
+++ config/db.js
@@ -1,5 +1,5 @@
 if(process.env.NODE_ENV == "production") {
-    module.exports = {mongoURI: "mongodb+srv://dbuser:verteiros2@bloapp-prod.htvwu.mongodb.net/<dbname>?retryWrites=true&w=majority"}
+    module.exports = {mongoURI: "mongodb+srv://dbUser:verteiros2@bloapp-prod.htvwu.mongodb.net/<dbname>?retryWrites=true&w=majority"}
 } else {
     module.exports = {mongoURI: "mongodb://localhost/blogapp"}
 }
